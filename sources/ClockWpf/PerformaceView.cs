1d7c256a6ff422ea2fecc0820a34fef5c12ff399
Alexandru Iuga <alex.iuga@gmail.com>
2026-01-15 15:09:43 +0200

    Fixed the performance view.

--- sources/ClockWpf/PerformaceView.cs
+++ sources/ClockWpf/PerformaceView.cs
@@ -3,12 +3,15 @@ using System.Windows.Controls;
 using System.Windows;
 using System.Windows.Media;
 using System.ComponentModel;
+using System.Reflection;
 
 namespace DustInTheWind.ClockWpf;
 
 public class PerformaceView : Control
 {
+    private AnalogClock currentAnalogClock;
     private PerformanceInfo currentPerformanceInfo;
+    private PropertyInfo performanceInfoProperty;
 
     protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
     {
@@ -16,14 +19,79 @@ public class PerformaceView : Control
 
         if (e.Property == DataContextProperty)
         {
-            if (currentPerformanceInfo != null)
-                currentPerformanceInfo.PropertyChanged -= OnPerformanceInfoChanged;
+            UnsubscribeFromCurrentPerformanceInfo();
+            UnsubscribeFromCurrentAnalogClock();
 
-            currentPerformanceInfo = DataContext as PerformanceInfo;
+            currentAnalogClock = null;
+            currentPerformanceInfo = null;
+            performanceInfoProperty = null;
+
+            if (DataContext is AnalogClock analogClock)
+            {
+                currentAnalogClock = analogClock;
+                performanceInfoProperty = analogClock.GetType().GetProperty("PerformanceInfo");
+
+                if (performanceInfoProperty != null)
+                {
+                    SubscribeToAnalogClockPropertyChanges();
+                    UpdatePerformanceInfo();
+                }
+            }
+        }
+    }
+
+    private void SubscribeToAnalogClockPropertyChanges()
+    {
+        if (currentAnalogClock != null)
+        {
+            DependencyPropertyDescriptor descriptor = DependencyPropertyDescriptor.FromName(
+                "PerformanceInfo",
+                typeof(AnalogClock),
+                typeof(AnalogClock));
+
+            if (descriptor != null)
+                descriptor.AddValueChanged(currentAnalogClock, OnAnalogClockPerformanceInfoChanged);
+        }
+    }
+
+    private void UnsubscribeFromCurrentAnalogClock()
+    {
+        if (currentAnalogClock != null)
+        {
+            DependencyPropertyDescriptor descriptor = DependencyPropertyDescriptor.FromName(
+                "PerformanceInfo",
+                typeof(AnalogClock),
+                typeof(AnalogClock));
+
+            if (descriptor != null)
+                descriptor.RemoveValueChanged(currentAnalogClock, OnAnalogClockPerformanceInfoChanged);
+        }
+    }
+
+    private void OnAnalogClockPerformanceInfoChanged(object sender, EventArgs e)
+    {
+        UpdatePerformanceInfo();
+    }
+
+    private void UpdatePerformanceInfo()
+    {
+        UnsubscribeFromCurrentPerformanceInfo();
+
+        if (performanceInfoProperty != null && currentAnalogClock != null)
+        {
+            currentPerformanceInfo = performanceInfoProperty.GetValue(currentAnalogClock) as PerformanceInfo;
 
             if (currentPerformanceInfo != null)
                 currentPerformanceInfo.PropertyChanged += OnPerformanceInfoChanged;
         }
+
+        InvalidateVisual();
+    }
+
+    private void UnsubscribeFromCurrentPerformanceInfo()
+    {
+        if (currentPerformanceInfo != null)
+            currentPerformanceInfo.PropertyChanged -= OnPerformanceInfoChanged;
     }
 
     private void OnPerformanceInfoChanged(object sender, PropertyChangedEventArgs e)
@@ -35,9 +103,9 @@ public class PerformaceView : Control
     {
         base.OnRender(drawingContext);
 
-        if (DataContext is PerformanceInfo performanceInfo)
+        if (currentPerformanceInfo != null)
         {
-            string performanceText = performanceInfo.ToString();
+            string performanceText = currentPerformanceInfo.ToString();
             FormattedText formattedText = new(
                 performanceText,
                 CultureInfo.CurrentCulture,
