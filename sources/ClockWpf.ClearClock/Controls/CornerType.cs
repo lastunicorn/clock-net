0e19574e82fa37f0439edec992f0cdf063d856b9
Alexandru Iuga <alex.iuga@gmail.com>
2026-01-18 21:41:57 +0200

    Removed the WPF projects.

--- sources/ClockWpf.ClearClock/Controls/CornerType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DustInTheWind.ClockWpf.ClearClock.Controls;
-
-public enum CornerType
-{
-    TopLeft,
-    TopRight,
-    BottomLeft,
-    BottomRight
-}
--- sources/ClockWpf/Shapes/IHand.cs
+++
@@ -1,8 +0,0 @@
-namespace DustInTheWind.ClockWpf.Shapes;
-
-public interface IHand
-{
-    double Length { get; set; }
-
-    TimeComponent ComponentToDisplay { get; set; }
-}
